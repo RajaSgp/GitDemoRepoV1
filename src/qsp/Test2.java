8a5400c02e4a2a71b51cba83150d965b3e4a7e7f
RajaSgp <raj10101990@gmail.com>
2022-03-05 10:55:37 +0530

    Test2 committ to BranchX...

---
+++ src/qsp/Test2.java
@@ -0,0 +1,15 @@
+package qsp;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+	}
+
+}
